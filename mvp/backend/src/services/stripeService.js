36e34893a54bc2d72a75db4890bfda90f01e994c
yatchy4-creator <yatchy4@gmail.com>
2026-03-10 20:44:39 -0400

    Set Stripe checkout payment method type to card

--- mvp/backend/src/services/stripeService.js
+++ mvp/backend/src/services/stripeService.js
@@ -11,6 +11,7 @@ export async function createCheckoutSession({ customerEmail, priceId, successUrl
   return stripe.checkout.sessions.create({
     mode: 'subscription',
     ...(customerEmail ? { customer_email: customerEmail } : {}),
+    payment_method_types: ['card'],
     line_items: [{ price: priceId, quantity: 1 }],
     success_url: successUrl,
     cancel_url: cancelUrl
